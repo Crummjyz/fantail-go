7b5f39b49d814e160c20be7a37bce829cc0f21db
Fantail-Dev <contact@fantail.dev>
2019-11-20 15:19:31 +1300

    Add dash

---
+++ dash/android/app/src/main/java/com/example/fantail_go_dash/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fantail_go_dash;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
